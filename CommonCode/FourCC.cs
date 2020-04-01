49670093a05ee7d5f9c59fca73d39905fabf56d2
Robin Hood <robinhood70@live.ca>
2020-04-01 02:48:35 -0400

    Add FourCC

---
+++ CommonCode/FourCC.cs
@@ -0,0 +1,15 @@
+﻿namespace RobinHood70.CommonCode
+{
+	using System;
+	using System.Globalization;
+	using System.Text;
+
+	public static class FourCC
+	{
+		public static string HexString(string text) => "0x" + Value(text).ToString("X8", CultureInfo.InvariantCulture);
+
+		public static uint Value(string text) => Value(Encoding.ASCII.GetBytes(text));
+
+		public static uint Value(byte[] bytes) => BitConverter.ToUInt32(bytes, 0);
+	}
+}
