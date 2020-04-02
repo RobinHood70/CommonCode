dc45dbae3a6ce8460c0ea8df029b7c3ad601106a
Robin Hood <robinhood70@live.ca>
2020-04-02 04:28:44 -0400

    Fix warnings

--- CommonCodeTester/App.xaml.cs
+++ CommonCodeTester/App.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace RobinHood70.CommonCodeTester
+﻿namespace RobinHood70.CommonCodeTester
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
+	using System.Windows;
+
+	/// <summary>Interaction logic for App.xaml.</summary>
 	public partial class App : Application
 	{
 	}
