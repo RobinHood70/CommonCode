3bd0f8327df828a1c5e3c95c8606ac8c911ce3c9
Robin Hood <robinhood70@live.ca>
2020-12-03 03:08:34 -0500

    Provide access to encoding type

--- CommonCode/FourCC.cs
+++ CommonCode/FourCC.cs
@@ -6,12 +6,16 @@
 
 	public static class FourCC
 	{
+		#region Public Static Properties
+		public static Encoding DefaultEncoding { get; set; } = Encoding.ASCII;
+
 		public static string HexString(string text) => "0x" + Value(text).ToString("X8", CultureInfo.InvariantCulture);
 
-		public static string ToString(uint value) => Encoding.ASCII.GetString(BitConverter.GetBytes(value));
+		public static string ToString(uint value) => DefaultEncoding.GetString(BitConverter.GetBytes(value));
 
-		public static uint Value(string text) => Value(Encoding.ASCII.GetBytes(text));
+		public static uint Value(string text) => Value(DefaultEncoding.GetBytes(text));
 
 		public static uint Value(byte[] bytes) => BitConverter.ToUInt32(bytes, 0);
+		#endregion
 	}
 }
