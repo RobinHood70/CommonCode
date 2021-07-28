d77c84ca73433e1ec1ea8252ddd91477d97f8539
Robin Hood <robinhood70@live.ca>
2021-07-27 23:06:11 -0400

    Ugh!

--- CommonCode/Globals.cs
+++ CommonCode/Globals.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
+	using System.Diagnostics.CodeAnalysis;
 	using System.Globalization;
 	using System.IO;
 	using System.Runtime.CompilerServices;
