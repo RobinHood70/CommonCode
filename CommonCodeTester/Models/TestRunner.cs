b7d820ef1b25b194666417da41923102c0c8347a
Robin Hood <robinhood70@live.ca>
2022-09-06 17:51:22 -0400

    Remove pointless IStringBuilder

--- CommonCodeTester/Models/TestRunner.cs
+++ CommonCodeTester/Models/TestRunner.cs
@@ -2,9 +2,7 @@
 {
 	using System;
 	using System.Diagnostics;
-	using System.Text;
 	using RobinHood70.CommonCode;
-	using RobinHood70.CommonCodeTester.Tests;
 	using RobinHood70.CommonCodeTester.Views;
 
 	public static class TestRunner
@@ -30,19 +28,6 @@
 
 		public static void RunTests()
 		{
-			IStringBuilder sbClass = new StringBuilderTest1();
-
-			Debug.WriteLine("== Class 1 ==");
-			Debug.WriteLine(sbClass);
-			Debug.WriteLine(sbClass.ToString());
-			Debug.WriteLine(sbClass.BuildString(new StringBuilder()));
-
-			sbClass = new StringBuilderTest2();
-			Debug.WriteLine("\n== Class 2 ==");
-			Debug.WriteLine(sbClass);
-			Debug.WriteLine(sbClass.ToString());
-			Debug.WriteLine(sbClass.BuildString(new StringBuilder()));
-
 			const string testText = "ON-trailer-Gates of Oblivion-Eveli.jpg";
 			Debug.WriteLine(testText.UnCamelCase());
 
